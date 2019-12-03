2a547af86aa6e0ffd1bc1dcc22ef158d10313333
iamyuvi2000 <yuvraj.singh2018@vitstudent.ac.in>
2019-12-03 14:22:14 +0530

    Using nested structs

--- Structs/main.go
+++ Structs/main.go
@@ -2,12 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	yuvraj := person{firstName: "Yuvraj", lastName: "Singh"}
+	yuvraj := person{
+		firstName: "Yuvraj",
+		lastName:  "Singh",
+		contact: contactInfo{
+			email:   "yuvi@gmail.com",
+			zipCode: 6500013,
+		},
+	}
 	fmt.Println(yuvraj)
 }
